3cbf0c59d1fe1505940e62e26020b03319d7014a
Lande <laande.pro@gmail.com>
2025-12-30 13:31:27 +0100

    switch icons to svg

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,6 @@
 import { defineConfig } from 'astro/config';
 
-import icon from 'astro-icon';
-
 export default defineConfig({
   site: 'https://laande.github.io/',
   base: '/',
-  integrations: [icon()]
 });
